4e93b1681473ef4386086022cdfec41de852c3f0
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-06-25 14:24:35 +0300

    Dynamic update ready.

--- src/index.js
+++ src/index.js
@@ -55,14 +55,25 @@ AFRAME.registerComponent('terrain', {
         let data = this.data;
         let el = this.el;
 
+        const step = data.step;
         this.radius = data.radius;
         this.radiusSquared = this.radius * this.radius;
 
-        this.updateDistanceSquared = (this.radius / 4) * (this.radius / 4)
-        const step = data.step;
+        this.vertices = new Array(2 * this.radius + 1);
+        this.vertexIndices = new Array(2 * this.radius + 1);
+        this.primaryFaces = new Array(2 * this.radius + 1);
+        this.secondaryFaces = new Array(2 * this.radius + 1);
+        for (var i = 0; i < 2 * this.radius + 1; i++) {
+            this.vertices[i] = new Array(2 * this.radius + 1);
+            this.vertexIndices[i] = new Array(2 * this.radius + 1);
+            this.primaryFaces[i] = new Array(2 * this.radius + 1);
+            this.secondaryFaces[i] = new Array(2 * this.radius + 1);
+        }
 
-        let getHeight = window.TINY_TERRAIN.heightFunctions.get(data.heightFunction)
-        let getColor = window.TINY_TERRAIN.colorFunctions.get(data.colorFunction)
+        this.terrainUpdateDistanceSquared = Math.pow(this.radius / 4, 2);
+
+        let getHeight = window.TINY_TERRAIN.heightFunctions.get(data.heightFunction);
+        let getColor = window.TINY_TERRAIN.colorFunctions.get(data.colorFunction);
 
         const paletteRangeMin = data.paletteRangeMin;
         const paletteRangeMax = data.paletteRangeMax;
@@ -72,8 +83,23 @@ AFRAME.registerComponent('terrain', {
         const colorPalette = interpolate(palette);
         const colorCache = new Map();
 
-        let getVector3 = (i, j, cx, cy, cz) => {
-            return new THREE.Vector3(cx + i * ui + j * ui / 2, cy + getHeight(cx + i * ui + j * ui / 2,cz + j * uj), cz + j * uj)
+        this.geometry = new THREE.Geometry();
+
+        let vi = 0
+        let pushVertice = (i, j, cx, cy, cz, add) => {
+            if (!this.vertices[i + this.radius][j + this.radius]) {
+                const vertice = new THREE.Vector3(cx + i * ui + j * ui / 2, cy + getHeight(cx + i * ui + j * ui / 2,cz + j * uj), cz + j * uj);
+                this.vertices[i + this.radius][j + this.radius] = vertice;
+                this.geometry.vertices.push(vertice)
+                this.vertexIndices[i + this.radius][j + this.radius] = vi;
+                vi++;
+            } else {
+                if (!add) {
+                    this.vertices[i + this.radius][j + this.radius].x = cx + i * ui + j * ui / 2;
+                    this.vertices[i + this.radius][j + this.radius].y = getHeight(cx + i * ui + j * ui / 2, cz + j * uj);
+                    this.vertices[i + this.radius][j + this.radius].z = cz + j * uj;
+                }
+            }
         };
 
         let getTriangleDistanceSquared = (i, j, step, primary) => {
@@ -84,76 +110,85 @@ AFRAME.registerComponent('terrain', {
             }
         };
 
-        let addFace = (v, i, j, cx, cy, cz, step, primary) => {
+
+        let pushTriangleVertices = (i, j, cx, cy, cz, step, primary, add) => {
             if (primary) {
-                this.geometry.vertices.push(
-                    getVector3(i, j, cx, cy, cz),
-                    getVector3(i, j + step, cx, cy, cz),
-                    getVector3(i + step, j, cx, cy, cz)
-                );
+                pushVertice(i, j, cx, cy, cz, add);
+                pushVertice(i, j + step, cx, cy, cz, add);
+                pushVertice(i + step, j, cx, cy, cz, add);
             } else {
-                this.geometry.vertices.push(
-                    getVector3(i, j + step, cx, cy, cz),
-                    getVector3(i + step, j + step, cx, cy, cz),
-                    getVector3(i + step, j, cx, cy, cz)
-                );
+                pushVertice(i, j + step, cx, cy, cz, add);
+                pushVertice(i + step, j + step, cx, cy, cz, add);
+                pushVertice(i + step, j, cx, cy, cz, add);
+            }
+        }
+
+        let pushTriangleFace = (i, j, cx, cy, cz, step, primary, add) => {
+            if (primary) {
+                let vi0 = this.vertexIndices[i + this.radius][j + this.radius];
+                let vi1 = this.vertexIndices[i + this.radius][j + step + this.radius];
+                let vi2 = this.vertexIndices[i + step + this.radius][j + this.radius];
+                let face = add ? new THREE.Face3(vi0, vi1, vi2) : this.primaryFaces[i + this.radius][j + this.radius];
+                face.vertexColors[0] = new THREE.Color(getColor(this.geometry.vertices[vi0].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
+                face.vertexColors[1] = new THREE.Color(getColor(this.geometry.vertices[vi1].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
+                face.vertexColors[2] = new THREE.Color(getColor(this.geometry.vertices[vi2].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
+                if (add) {
+                    this.geometry.faces.push(face);
+                    this.primaryFaces[i + this.radius][j + this.radius] = face
+                }
+            } else {
+                let vi0 = this.vertexIndices[i + this.radius][j + step + this.radius];
+                let vi1 = this.vertexIndices[i + step + this.radius][j + step + this.radius];
+                let vi2 = this.vertexIndices[i + step + this.radius][j + this.radius];
+                let face = add ? new THREE.Face3(vi0, vi1, vi2) : this.secondaryFaces[i + this.radius][j + this.radius];
+                face.vertexColors[0] = new THREE.Color(getColor(this.geometry.vertices[vi0].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
+                face.vertexColors[1] = new THREE.Color(getColor(this.geometry.vertices[vi1].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
+                face.vertexColors[2] = new THREE.Color(getColor(this.geometry.vertices[vi2].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
+                if (add) {
+                    this.geometry.faces.push(face);
+                    this.secondaryFaces[i + this.radius][j + this.radius] = face
+                }
             }
 
-            let face = new THREE.Face3(v + 0, v + 1, v + 2);
-            face.vertexColors[0] = new THREE.Color(getColor(this.geometry.vertices[v + 0].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
-            face.vertexColors[1] = new THREE.Color(getColor(this.geometry.vertices[v + 1].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
-            face.vertexColors[2] = new THREE.Color(getColor(this.geometry.vertices[v + 2].y - cy, colorPalette, paletteRangeMin, paletteRangeMax, paletteAccuracy, colorCache));
-            this.geometry.faces.push(face);
         };
 
-        this.updateTerrain = (cx, cy, cz) =>
+        this.updateTerrain = (cx, cy, cz, add) =>
         {
-            this.geometry = new THREE.Geometry();
-
-            let v = 0;
             for (let i = -this.radius; i < this.radius; i += step) {
                 for (let j = -this.radius; j < this.radius; j += step) {
                     if (getTriangleDistanceSquared(i, j, step, true) <= this.radiusSquared) {
-                        addFace(v, i, j, cx, cy, cz, step, true);
-                        v += 3;
+                        pushTriangleVertices(i, j, cx, cy, cz, step, true, add);
+                        pushTriangleFace(i, j, cx, cy, cz, step, true, add);
                     }
                     if (getTriangleDistanceSquared(i, j, step, false) <= this.radiusSquared) {
-                        addFace(v, i, j, cx, cy, cz, step, false);
-                        v += 3;
+                        pushTriangleVertices(i, j, cx, cy, cz, step, false, add);
+                        pushTriangleFace(i, j, cx, cy, cz, step, false, add);
                     }
                 }
             }
 
-            this.geometry.mergeVertices();
             this.geometry.computeFaceNormals();
             this.geometry.computeVertexNormals();
+            this.geometry.verticesNeedUpdate = true;
+            this.geometry.elementsNeedUpdate = true;
 
-            this.material = new THREE.MeshLambertMaterial({color: 0xffffff, vertexColors: THREE.VertexColors});
+            if (add) {
 
-            this.wireMaterial = new THREE.MeshLambertMaterial({
-                color: 0xdddddd,
-                wireframe: true,
-                vertexColors: THREE.VertexColors
-            });
+                this.material = new THREE.MeshLambertMaterial({color: 0xffffff, vertexColors: THREE.VertexColors});
 
-            if (this.mesh) {
-                el.removeObject3D('mesh');
-                this.mesh.geometry.dispose();
-                this.mesh.material.dispose();
-                this.mesh = undefined;
-            }
-            if (this.wire) {
-                el.removeObject3D('wire');
-                this.wire.geometry.dispose();
-                this.wire.material.dispose();
-                this.wire = undefined;
+                this.wireMaterial = new THREE.MeshLambertMaterial({
+                    color: 0xdddddd,
+                    wireframe: true,
+                    vertexColors: THREE.VertexColors
+                });
+
+                this.mesh = new THREE.Mesh(this.geometry, this.material);
+                this.wire = new THREE.Mesh(this.geometry, this.wireMaterial);
+
+                el.setObject3D('mesh', this.mesh);
+                el.setObject3D('wire', this.wire);
             }
 
-            this.mesh = new THREE.Mesh(this.geometry, this.material);
-            this.wire = new THREE.Mesh(this.geometry, this.wireMaterial);
-
-            el.setObject3D('mesh', this.mesh);
-            el.setObject3D('wire', this.wire);
         }
 
         const cx = data.x;
@@ -162,7 +197,7 @@ AFRAME.registerComponent('terrain', {
         this.cx = cx;
         this.cy = cy;
         this.cz = cz;
-        this.updateTerrain(cx, cy, cz)
+        this.updateTerrain(cx, cy, cz, true)
     },
 
     update: function (oldData) {
@@ -172,12 +207,12 @@ AFRAME.registerComponent('terrain', {
         const cy = data.y;
         const cz = (data.z / (4 * uj)).toFixed() * 4 * uj;
 
-        if ((this.cx - cx) * (this.cx - cx) + (this.cz - cz) * (this.cz - cz) >= this.updateDistanceSquared) {
+        if ((this.cx - cx) * (this.cx - cx) + (this.cz - cz) * (this.cz - cz) >= this.terrainUpdateDistanceSquared) {
             console.log('recalculated terrain at : ' + this.data.x + ',' + this.data.y)
             this.cx = cx;
             this.cy = cy;
             this.cz = cz;
-            this.updateTerrain(cx, cy, cz)
+            this.updateTerrain(cx, cy, cz, false)
         }
     }
 
